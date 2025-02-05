a7e968567f33b956dd7141ded49603745e889a59
Kieran Bondy <52510822+kieranbondy@users.noreply.github.com>
2025-02-05 11:13:45 -0500

    fonts

--- app/entry.server.jsx
+++ app/entry.server.jsx
@@ -22,6 +22,8 @@ export default async function handleRequest(
       checkoutDomain: context.env.PUBLIC_CHECKOUT_DOMAIN,
       storeDomain: context.env.PUBLIC_STORE_DOMAIN,
     },
+    styleSrc: ["'self'", 'https://use.typekit.net', 'https://p.typekit.net'],
+    fontSrc: ["'self'", 'https://use.typekit.net', 'https://p.typekit.net'], // Allow fonts
   });
 
   const body = await renderToReadableStream(
